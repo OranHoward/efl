842b108acb1fbf51e21a7f5be0f8652fb0c1be45
OranHoward <oranhoward1@gmail.com>
2022-08-07 13:51:59 +0100

    initial commit

---
+++ src/data_collection.py
@@ -0,0 +1,4 @@
+import pandas
+
+def read_data():
+    fifa_results_final = pd.read_csv('Final_data/fifa_results_final.csv')
---
+++ src/sys_startup.py
@@ -0,0 +1,7 @@
+from data_collection import * 
+
+def __main__():
+
+# things to run when program starts up
+
+    read_data()
